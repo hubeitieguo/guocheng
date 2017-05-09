0562215a725827f3f764bd055f7f21ecdb761bda
hubeitieguo <1945166500@qq.com>
2017-05-09 18:18:03 +0800

    hj
    Signed-off-by: hubeitieguo <1945166500@qq.com>

--- src/net/guocheng/cheer/sample/ch01/HelloJava.java
+++ src/net/guocheng/cheer/sample/ch01/HelloJava.java
@@ -1,5 +1,10 @@
 package net.guocheng.cheer.sample.ch01;
 
 public class HelloJava {
-
+	static String s1="你好";
+	public static void main (String[] args){
+		String s2="java";
+		System.out.println(s1);
+		System.out.println(s2);
+	}
 }
